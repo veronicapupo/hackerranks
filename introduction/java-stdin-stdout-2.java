1c90a7625e6590f2fcc1ed2057bfc3087891f3a8
Verônica Pupo <105063311+veronicapupo@users.noreply.github.com>
2022-07-27 18:50:21 -0300

    solution

---
+++ introduction/java-stdin-stdout-2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+      Scanner scanner = new Scanner(System.in);
+      
+      int i = scanner.nextInt();
+      double d = scanner.nextDouble();
+      scanner.nextLine();
+      String s = scanner.nextLine();
+      scanner.close();
+      
+      System.out.println("String: " + s);
+      System.out.println("Double: " + d);
+      System.out.println("Int: " + i);
+    }
+}
